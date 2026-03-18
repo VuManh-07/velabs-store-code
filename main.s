b0cc715f94a9e5251dcd103ad4159e81c149de3e
Vũ Đức Mạnh <91170967+VuManh-07@users.noreply.github.com>
2026-03-18 16:21:13 +0700

    B21DCDT048

--- main.s
+++ main.s
@@ -1,14 +1,3 @@
-.syntax unified
-.cpu cortex-m3
-.thumb
+// Nhập nội dung trả
 
-.word 0x20000400
-.word 0x080000ed
-.space 0xe4
-
-mov r0, #65
-movw r1, #32795
-ldr r2, =539230465
-B .
-
-//Random_code:2uligxnb7zw
+//Random_code:aqhdhocq9d
